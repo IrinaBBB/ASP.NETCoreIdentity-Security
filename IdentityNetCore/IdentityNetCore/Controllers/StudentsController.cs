eea61bda340724925b06c5ef1ea728bf5d6caca3
IrinaB <balkarovairina@gmail.com>
2024-03-29 17:13:15 +0100

    Add a Method for Testing Common Security Risks

---
+++ IdentityNetCore/IdentityNetCore/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityNetCore.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    // .Net adds antiforgery token automatically where needed
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // use this annotation where the form will change the "state" of the app
+        public IActionResult SignUp(StudentViewModel model) 
+        {
+            return View("Result", model);
+        }
+    }
+}
